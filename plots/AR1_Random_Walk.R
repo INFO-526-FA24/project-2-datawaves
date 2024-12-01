6c4864b871949186fa658922511b2e2c75f5d0d9
Saharsh Bhave <saharshsbhave@arizona.edu>
2024-12-01 16:37:24 -0700

    modified files to add points and eqs.

--- plots/AR1_Random_Walk.R
+++ plots/AR1_Random_Walk.R
@@ -9,7 +9,9 @@ plot_data <- data.frame(Time = 1:201, Value = random_walk_arima)
 # Plotting the random walk (ARIMA(0,1,0))
 ggplot(plot_data, aes(x = Time, y = Value)) + 
   geom_line(color = "red") + 
+  geom_point(color  = "red")+
   labs(title = "Simulated Random Walk using ARIMA(0,1,0)",
+       subtitle = ~  Y[t] == Y[t-1] + e[t],
        x = "Time",
        y = "Value") + 
   theme_minimal()
--- plots/AR1_with_random_walk.R
+++ plots/AR1_with_random_walk.R
@@ -3,9 +3,10 @@ set.seed(123)
 
 # Using arima.sim to simulate ARIMA(0,1,0) (Random Walk)
 random_walk_arima <- arima.sim(model = list(order = c(0, 1, 0)), n = 199)
+#n for random walk is 1 less as  
 
 # Using arima.sim to simulate AR(1)
-ar1_process<- arima.sim(model = list(ar = 0.6), n = 200)
+ar1_process<- arima.sim(model = list(ar = 0.8), n = 200)
 
 #plotting
 df <- data.frame(
@@ -16,7 +17,8 @@ df <- data.frame(
 
 ggplot(df, aes(x = Time, y = Value, color = Process)) + 
   geom_line() + 
-  labs(title = "Random Walk vs. AR(1) Process",
+  geom_point()+
+  labs(title = "Random Walk vs. AR(1) Process", 
        x = "Time",
        y = "Value",
        color = "Process") +  
